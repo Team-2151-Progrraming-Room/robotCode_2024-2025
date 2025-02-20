59d5293fc066f16f7c0d7241032fa38fffd4c2cb
Chris <chrisrivera.5128@gmail.com>
2025-02-20 16:31:41 -0600

    added some stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
@@ -44,12 +46,25 @@ public class RobotContainer {
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
+  private final Joystick buttonBoard = new Joystick(port:1);
+
+  public final JoystickButton button1;
+  public final JoystickButton button2;
+  public final JoystickButton button3;
+  public final JoystickButton button4;
+  public final JoystickButton button5;
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    button1 = new JoystickButton(buttonBoard, 1);
+    button2 = new JoystickButton(buttonBoard, 2);
+    button3 = new JoystickButton(buttonBoard, 3);
+    button4 = new JoystickButton(buttonBoard, 4);
+    button5 = new JoystickButton(buttonBoard, 5);
+
     switch (Constants.currentMode) {
       case REAL:
         // Real robot, instantiate hardware IO implementations
@@ -146,6 +161,7 @@ public class RobotContainer {
                             new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
                     drive)
                 .ignoringDisable(true));
+    
   }
 
   /**
