9036b741138a564739fad1823efc7cb5f0a1d294
NubiaZ <nubiaxz@icloud.com>
2025-02-22 12:13:58 -0600

    deleted drive subsystem and all drive related files from arm subsystem branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,17 +23,16 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.GyroIO;
-import frc.robot.subsystems.drive.GyroIOPigeon2;
-import frc.robot.subsystems.drive.ModuleIO;
-import frc.robot.subsystems.drive.ModuleIOSim;
-import frc.robot.subsystems.drive.ModuleIOTalonFX;
+import java.util.function.BooleanSupplier;
+
+//our subsystems
 import frc.robot.subsystems.ArmSubsystemCTRE;
+
+
+//out commands
+
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -44,13 +43,10 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
   // Subsystems
-  private final Drive drive;
-  private final ArmSubsystemCTRE arm;
+  private final ArmSubsystemCTRE arm = new ArmSubsystemCTRE();
 
   // Controller
   private final CommandXboxController controller = new CommandXboxController(0);
-
-  //buttonBoard
   private final Joystick buttonBoard = new Joystick(1);
 
   public final JoystickButton button1;
@@ -58,14 +54,6 @@ public class RobotContainer {
   public final JoystickButton button3;
   public final JoystickButton button4;
   public final JoystickButton button5;
-  public final JoystickButton button6;
-  public final JoystickButton button7;
-  public final JoystickButton button8;
-  public final JoystickButton button9;
-  public final JoystickButton button10;
-  public final JoystickButton button11;
-  public final JoystickButton button12;
-
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
@@ -75,73 +63,13 @@ public class RobotContainer {
     button1 = new JoystickButton(buttonBoard, 1);
     button2 = new JoystickButton(buttonBoard, 2);
     button3 = new JoystickButton(buttonBoard, 3);
-    button4 = new JoystickButton(buttonBoard,4);
-    button5 = new JoystickButton(buttonBoard,5);
-    button6 = new JoystickButton(buttonBoard, 6);
-    button7 = new JoystickButton(buttonBoard, 7);
-    button8 = new JoystickButton(buttonBoard, 8);
-    button9 = new JoystickButton(buttonBoard, 9);
-    button10 = new JoystickButton(buttonBoard, 10);
-    button11 = new JoystickButton(buttonBoard, 11);
-    button12 = new JoystickButton(buttonBoard, 12);
+    button4 = new JoystickButton(buttonBoard, 4);
+    button5 = new JoystickButton(buttonBoard, 5);
 
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive =
-            new Drive(
-                new GyroIOPigeon2(),
-                new ModuleIOTalonFX(TunerConstants.FrontLeft),
-                new ModuleIOTalonFX(TunerConstants.FrontRight),
-                new ModuleIOTalonFX(TunerConstants.BackLeft),
-                new ModuleIOTalonFX(TunerConstants.BackRight));
-        break;
-
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIOSim(TunerConstants.FrontLeft),
-                new ModuleIOSim(TunerConstants.FrontRight),
-                new ModuleIOSim(TunerConstants.BackLeft),
-                new ModuleIOSim(TunerConstants.BackRight));
-        break;
-
-      default:
-        // Replayed robot, disable IO implementations
-        drive =
-            new Drive(
-                new GyroIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {},
-                new ModuleIO() {});
-        break;
-    }
 
     // Set up auto routines
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-
-    //arm subsystem
-    arm = new ArmSubsystemCTRE();
-
-    // Set up SysId routines
-    autoChooser.addOption(
-        "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
-    autoChooser.addOption(
-        "Drive Simple FF Characterization", DriveCommands.feedforwardCharacterization(drive));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-
+  
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -153,45 +81,6 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    // Default command, normal field-relative drive
-    drive.setDefaultCommand(
-        DriveCommands.joystickDrive(
-            drive,
-            () -> -controller.getLeftY(),
-            () -> -controller.getLeftX(),
-            () -> -controller.getRightX()));
-
-    // Lock to 0° when A button is held
-    controller
-        .a()
-        .whileTrue(
-            DriveCommands.joystickDriveAtAngle(
-                drive,
-                () -> -controller.getLeftY(),
-                () -> -controller.getLeftX(),
-                () -> new Rotation2d()));
-
-    // Switch to X pattern when X button is pressed
-    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
-
-    // Reset gyro to 0° when B button is pressed
-    controller
-        .b()
-        .onTrue(
-            Commands.runOnce(
-                    () ->
-                        drive.setPose(
-                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-                    drive)
-                .ignoringDisable(true));
-
-    /*
-     * Binds each button on the button board with it's command
-     * Some call it directly from the subsystem class (manual arm commands),
-     * While others call the command from the file
-     */
-    button1.whileTrue(arm.armManualUpCommand());
-    button2.whileTrue(arm.armManualDownCommand());
   }
 
   /**
